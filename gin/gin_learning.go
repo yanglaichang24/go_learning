232f57c46599530272db0b0c6b33d6697ff57b88
NeoYong <yanglaichang24@gmail.com>
2024-04-17 18:01:57 +0800

    update

--- gin/gin_learning.go
+++ gin/gin_learning.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
-
 	r := gin.Default()
-	irout := r.GET("/ping", func(c *gin.Context) {
+
+	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	println(irout)
-	r.Run(":8083") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.GET("/text", func(c *gin.Context) {
+		_, err := c.Writer.WriteString("xxxxxxxxxxstre")
+		if err != nil {
+			return
+		}
+	})
+
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if err := r.Run(":8083"); err != nil {
+		fmt.Println("error ")
+	}
 }
 
 func pong(c *gin.Context) {
